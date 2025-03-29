052f127c42d58a3f69f757e2794f4de0dcfcfe75
Keniel Nunes <kenielalves3@gmail.com>
2025-03-29 18:58:45 -0300

    feat: initial config

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  },
+};
+
+export default nextConfig;
